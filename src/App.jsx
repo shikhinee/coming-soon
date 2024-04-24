7134c5781f695b0b01c357afda7880203858fd11
Shikhikhutug Tseren <shikhinee@gmail.com>
2024-04-24 02:59:25 -0700

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -13,14 +13,11 @@ function App() {
       <div className="flex flex-row box-border w-full justify-between h-16 z-50 px-6 py-8">
         <img className="h-8" src={Smart}/>
         <div className="flex flex-row gap-6">
-          <a className="text-white font-normal" href="">LinkedIn</a>
-          <a className="text-white font-normal" href="">Facebook</a>
-          <a className="text-white font-normal" href="">Instagram</a>
         </div>
       </div>
-      <div className="flex flex-col w-full h-full justify-center items-center gap-10 z-50 pb-16 box-border">
+      <div className="flex flex-col w-full h-full justify-center items-center gap-10 z-50 pb-16 box-border px-2">
         <h1 className="text-white md:text-[144px] text-[48px] tracking-tighter">Coming Soon</h1>
-        <p className="text-white text-[24px] opacity-[0.7]">Hello, our new website is currently under development.</p>
+        <p className="text-white text-center text-lg md:text-[24px] opacity-[0.7]">Hello, our new website is currently under development.</p>
       </div>
       </div>
     </div>
