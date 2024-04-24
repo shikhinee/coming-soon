ecc73c7ee7f877d00ea5cd7e0fba4db0b4ea3b6e
Shikhikhutug Tseren <shikhikhutug.ts@tenplus.mn>
2024-04-24 01:19:14 -0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import City from './assets/city.jpg'
+import Smart from "./assets/Smart.png"
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="max-h-screen max-w-screen relative box-border">
+      <img className="h-full w-full box-border absolute z-[-1] bg-cover" src={City}/>
+      <div className="w-full h-full absolute z-[-1] bg-gradient-to-t from-[#003D97]"/>
+      <div className="flex flex-col w-screen h-screen">
+      <div className="flex flex-row box-border w-full justify-between h-16 z-50 px-6 py-8">
+        <img className="h-8" src={Smart}/>
+        <div className="flex flex-row gap-6">
+          <a className="text-white font-normal" href="">LinkedIn</a>
+          <a className="text-white font-normal" href="">Facebook</a>
+          <a className="text-white font-normal" href="">Instagram</a>
+        </div>
+      </div>
+      <div className="flex flex-col w-full h-full justify-center items-center gap-10 z-50 pb-16 box-border">
+        <h1 className="text-white md:text-[144px] text-[48px] tracking-tighter">Coming Soon</h1>
+        <p className="text-white text-[24px] opacity-[0.7]">Hello, our new website is currently under development.</p>
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
